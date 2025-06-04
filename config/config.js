1cf068577459c4bc8ed169d1940025411ebfc522
isabell-ah <wanzalaisabella@gmail.com>
2025-06-04 18:27:03 +0300

    m

--- config/config.js
+++ config/config.js
@@ -15,7 +15,7 @@ const config = {
   server: {
     port: process.env.PORT || 3000,
     nodeEnv: process.env.NODE_ENV || 'development',
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+    baseUrl: process.env.BASE_URL,
     jwtSecret: process.env.JWT_SECRET || 'my_security',
     jwtExpiresIn: process.env.JWT_EXPIRES_IN || '1d',
   },
@@ -64,7 +64,7 @@ const config = {
   //     process.env.LNBITS_WALLET_ID || '56968c19adb64a389bc730823f63b116',
   // },
   lnbits: {
-    baseUrl: process.env.LNBITS_BASE_URL || 'https://demo.lnbits.com/api/v1',
+    baseUrl: process.env.LNBITS_BASE_URL,
     adminKey: process.env.LNBITS_ADMIN_KEY,
     invoiceKey: process.env.LNBITS_INVOICE_KEY,
     walletId: process.env.LNBITS_WALLET_ID,
